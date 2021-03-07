7cfb11d1c8d40877b4380f5439e9d36f1b29293d
Luthfi Khusyasy <Khusyasy@users.noreply.github.com>
2021-03-07 23:07:23 +0700

    add useFindAndModify deprecation

--- helpers/database.js
+++ helpers/database.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
