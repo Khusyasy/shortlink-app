01502495000c8a36550ab1e778286850ce749067
Luthfi Khusyasy <Khusyasy@users.noreply.github.com>
2021-03-07 15:11:42 +0700

    typo findOne

--- controllers/redir.js
+++ controllers/redir.js
@@ -5,11 +5,11 @@ const URLisValid = require("../helpers/url_validation");
 
 exports.redirToLink = async (req, res) => {
     try {
-        var link = await Link.findOe({ shortUrl: req.params.short });
+        var link = await Link.findOne({ shortUrl: req.params.short });
         if (URLisValid(link.longUrl)) {
             res.redirect(link.longUrl)
         }
     } catch (err) {
-        res.redirect("/");
+        res.redirect("/error");
     }
 }
